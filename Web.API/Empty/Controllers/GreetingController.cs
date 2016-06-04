f0434f367b4ee55bf44a3762bd56c87f558f58c9
Andreas Wänqvist <andrkarl@live.com>
2016-06-05 01:20:50 +0200

    more

---
+++ Web.API/Empty/Controllers/GreetingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Empty.Controllers
+{
+    [Route("api/[controller]")]
+    public class GreetingController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Greetings from Web API!";
+        }
+    }
+}
